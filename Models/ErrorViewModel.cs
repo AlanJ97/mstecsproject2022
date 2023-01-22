531d722c6a3462131794a081021e36d5ccf596b9
alan <alannavaseg_02@hotmail.com>
2023-01-22 15:38:29 -0600

    Creating asp.net mvc app with specific name

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace mstecsproject.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
